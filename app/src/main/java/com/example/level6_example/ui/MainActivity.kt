d0d36215315e6dd223e87767c9fa5813508f0347
@BrunoFigueiredo2k <brunofigueiredo@live.nl>
2020-11-14 14:00:57 +0100

    finished app

--- app/src/main/java/com/example/level6_example/ui/MainActivity.kt
+++ app/src/main/java/com/example/level6_example/ui/MainActivity.kt
@@ -1,23 +1,29 @@
 package com.example.level6_example.ui
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import android.widget.Toast.LENGTH_LONG
+import androidx.activity.viewModels
 import com.example.level6_example.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: TriviaViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        fab.setOnClickListener {
+            viewModel.getTriviaNumber()
         }
     }
 
